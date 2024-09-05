3ebd84acc93065ab70c2b8c83280c0b0ef2d50be
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-09-06 00:00:10 +0300

    Update Housing prices.py

--- Housing prices.py
+++ Housing prices.py
@@ -34,6 +34,19 @@ def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
     preds = model.predict(X_v)
     return mean_absolute_error(y_v, preds)
 
+
 for i in range(0, len(models)):
     mae = score_model(models[i])
     print("Model %d MAE: %d" % (i+1, mae))
+
+
+
+best_model = model_3
+best_model.fit(X, y)
+
+preds_test = best_model.predict(X_test)
+
+output = pd.DataFrame({'Id': X_test.index,
+                       'SalePrice': preds_test})
+output.to_csv('submission.csv', index=False)
+
