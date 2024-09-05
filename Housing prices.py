1e8181f9658e52aa7cbbc9b580eef185268340d5
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-09-05 23:29:01 +0300

    Add files via upload

---
+++ Housing prices.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+X_full = pd.read_csv('train.csv')
+X_test_full = pd.read_csv('test.csv')
+
+
+y = X_full.SalePrice
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = X_full[features].copy()
+X_test = X_test_full[features].copy()
+
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+
+print(X_train.head())
